e4a2420dfd558b7480794a7a4e219b8d30c89ec1
Ім'я корAnton-19 <a.v.oleshko@student.khai.edu>
2024-10-02 12:42:45 +0300

    Your commit message

--- test.py
+++ test.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
 print("Python education")
-# new string print
-("Hello DICT!")
-=======
-print("Python")
 # new string
-print(“DICT”)
->>>>>>> d78cdb5 (test.py change on string#3)
+("Hello DICT!")
