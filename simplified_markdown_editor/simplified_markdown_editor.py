dbf2adeeb7888c4926ea7b8adf19a91d0b82d383
Ім'я корAnton-19 <a.v.oleshko@student.khai.edu>
2024-11-18 12:14:58 +0200

    matrixprocessing 1-th stage
    matrixprocessing 3-th stage
    
    matrixprocessing 4-th stage
    
    matrixprocessing 5-th stage
    
    matrixprocessing 6-th stage
    
    simplified_markdown_editor 1-nd stage
    
    simplified_markdown_editor 2-nd stage
    
    simplified_markdown_editor 3-nd stage
    
    simplified_markdown_editor 5-rd stage

---
+++ simplified_markdown_editor/simplified_markdown_editor.py
@@ -0,0 +1,94 @@
+def print_help():
+    # Вивід доступних форматерів та спеціальних команд
+    print("Available formatters: plain bold italic header link inline-code ordered-list unordered-list new-line")
+    print("Special commands: !help !done")
+
+
+def apply_format(formatter, text):
+    # Застосування форматування до тексту
+    if formatter == "plain":
+        return text
+    elif formatter == "bold":
+        return f"**{text}**"
+    elif formatter == "italic":
+        return f"*{text}*"
+    elif formatter == "inline-code":
+        return f"`{text}`"
+    elif formatter == "header":
+        level = int(input("Level: > "))
+        if 1 <= level <= 6:
+            return f"{'#' * level} {text}\n"
+        else:
+            print("The level should be within the range of 1 to 6.")
+            return ""
+    elif formatter == "link":
+        url = input("URL: > ")
+        return f"[{text}]({url})"
+    elif formatter == "new-line":
+        return "\n"
+    return text
+
+
+def apply_list(formatter):
+    # Форматування списків
+    num_rows = int(input("Number of rows: > "))
+    if num_rows <= 0:
+        print("The number of rows should be greater than zero.")
+        return ""
+
+    list_items = []
+    for i in range(1, num_rows + 1):
+        item = input(f"Row #{i}: > ")
+        if formatter == "ordered-list":
+            list_items.append(f"{i}. {item}")
+        else:
+            list_items.append(f"* {item}")
+
+    return "\n".join(list_items) + "\n"
+
+
+def main():
+    # Набір доступних форматерів
+    available_formatters = {"plain", "bold", "italic", "header", "link", "inline-code", "ordered-list","unordered-list", "new-line"}
+    # Набір спеціальних команд
+    special_commands = {"!help", "!done"}
+    markdown_text = ""
+
+    while True:
+        # Запит користувача на вибір форматера
+        user_input = input("Choose a formatter: > ")
+
+        if user_input == "!help":
+            print_help()
+        elif user_input == "!done":
+            with open("output.md", "w", encoding="utf-8") as file:
+                file.write(markdown_text)
+            print(markdown_text)
+            break
+        elif user_input in available_formatters:
+            if user_input == "new-line":
+                markdown_text += "\n"
+            elif user_input == "header":
+                level = int(input("Level: > "))
+                if 1 <= level <= 6:
+                    text = input("Text: > ")
+                    markdown_text += f"{'#' * level} {text}\n"
+                else:
+                    print("The level should be within the range of 1 to 6.")
+            elif user_input == "link":
+                label = input("Label: > ")
+                url = input("URL: > ")
+                markdown_text += f"[{label}]({url})"
+            elif user_input in {"ordered-list", "unordered-list"}:
+                markdown_text += apply_list(user_input)
+            else:
+                text = input("Text: > ")
+                markdown_text += apply_format(user_input, text) + " "
+            print(markdown_text)
+        else:
+            print("Unknown formatting type or command")
+
+
+if __name__ == "__main__":
+    main()
+
